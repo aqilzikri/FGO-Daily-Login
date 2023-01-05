110e1f8285f10f324f8b9167ccb3e7f89f1fe191
Aidan Yusuf <119834868+aqilzikri@users.noreply.github.com>
2023-01-06 07:50:29 +0800

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -6,7 +6,7 @@ import fgourl
 
 def topLogin(data: list) -> None:
     endpoint = main.webhook_discord_url
-    SendMessageToAdmin(message)
+    fgourl.SendMessageToAdmin(message)
 
     rewards: user.Rewards = data[0]
     login: user.Login = data[1]
