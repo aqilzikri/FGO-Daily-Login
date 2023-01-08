db2194a72ddc39e1ff803cad190ba553836d4aab
Aidan Yusuf <119834868+aqilzikri@users.noreply.github.com>
2023-01-09 03:23:09 +0800

    Update user.py

--- user.py
+++ user.py
@@ -123,8 +123,7 @@ class user:
         data = self.Post(
             f'{fgourl.server_addr_}/login/top?_userId={self.user_id_}')
 
-        self.name_ = hashlib.md5(
-            data['cache']['replaced']['userGame'][0]['name'].encode('utf-8')).hexdigest()
+        self.name_ = data['cache']['replaced']['userGame'][0]['name']
         stone = data['cache']['replaced']['userGame'][0]['stone']
         lv = data['cache']['replaced']['userGame'][0]['lv']
         ticket = 0
